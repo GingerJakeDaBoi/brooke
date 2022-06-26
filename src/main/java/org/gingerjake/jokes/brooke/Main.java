ede243dda9a53e9ce66a01b553d3b6aa64ed7b02
GingerJakeDaBoi <GingerJakeDaBoi@users.noreply.github.com>
2022-06-26 00:52:12 -0400

    more insults

--- src/main/java/org/gingerjake/jokes/brooke/Main.java
+++ src/main/java/org/gingerjake/jokes/brooke/Main.java
@@ -3,14 +3,13 @@ package org.gingerjake.jokes.brooke;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Main {
     public Main() {
     }
 
-    public static <insults> void main(String[] args) throws UnknownHostException {
+    public static <insults> void main() throws UnknownHostException {
 
         //test that the app works
         System.out.println("Hello World!");
@@ -20,7 +19,7 @@ public class Main {
         System.out.println("Your IP address is... " + ip.getHostAddress());
 
         //make a new list for insults
-        List<insults> stList = new ArrayList();
+        ArrayList<insults> stList = new ArrayList<>();
 
         //All the insults (more later)
         stList.add((insults) "You're a piece of shit");
@@ -31,10 +30,17 @@ public class Main {
         stList.add((insults) "You'd probably let me peg you...");
         stList.add((insults) "You probably wet your bed every night.");
         stList.add((insults) "I don't know if you're an idiot or just stupid.");
+        stList.add((insults) "Please... Shut up.");
+        stList.add((insults) "How much do you weigh, again?");
+        stList.add((insults) "You didn't fuck Kalie, she fucked you!");
+        stList.add((insults) "small.mp4 LOL");
+        stList.add((insults) "gtfo rofl!");
 
         //Print a random insult from the list
         Random rand = new Random();
         int randomIndex = rand.nextInt(stList.size());
         System.out.println(stList.get(randomIndex));
+
+
     }
 }
